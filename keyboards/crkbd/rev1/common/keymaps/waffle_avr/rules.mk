c5dfe9c529c58c147bf05db3b002eab806ac9222
ItsWaffie <applej955@gmail.com>
2021-02-22 16:56:18 -0700

    Add common crkbd keymaps

---
+++ keyboards/crkbd/rev1/common/keymaps/waffle_avr/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE     = no     # Mouse keys
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = no         # Enable VIA
+OLED_DRIVER_ENABLE  = yes
+LTO_ENABLE          = yes
+WPM_ENABLE 			= no
