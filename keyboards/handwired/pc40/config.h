a0b3bcfa808938c76d9febfba487b3eeede80a8c
ItsWaffie <applej955@gmail.com>
2021-02-22 17:13:30 -0700

    Add rest of important keymaps

---
+++ keyboards/handwired/pc40/config.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "config_common.h"
+#define VENDOR_ID 0x1987
+#define PRODUCT_ID 0x8261
+#define DEVICE_VER 0x0001
+#define MANUFACTURER Waffle
+#define PRODUCT 4x12 Handwired Keyboard
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+#define MATRIX_ROW_PINS { B1, B0, B15, B9 }
+#define MATRIX_COL_PINS { B6, A2, B4, B3, B2, A1, A0, B8, B10, A4, A5, B11 }
+#define UNUSED_PINS
+#define DIODE_DIRECTION ROW2COL
+#define C6_AUDIO
+#define USB_POLLING_INTERVAL_MS 1
