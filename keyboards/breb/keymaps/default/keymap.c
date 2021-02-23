dddd9a0c0c47a10f1d0045b185534ec216b09873
toastedmangoes <applej955@gmail.com>
2021-02-23 11:18:01 -0700

    Add Tapdance Testing

--- keyboards/breb/keymaps/default/keymap.c
+++ keyboards/breb/keymaps/default/keymap.c
@@ -17,6 +17,31 @@
 #include QMK_KEYBOARD_H
 bool show_img = false;
 
+#ifdef TAP_DANCE_ENABLE
+typedef struct {
+    bool is_press_action;
+    uint8_t state;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    TRIPLE_TAP,
+    TRIPLE_HOLD
+};
+
+enum {
+    CAP_CTL
+};
+
+uint8_t cur_dance(qk_tap_dance_state_t *state);
+void cap_finished(qk_tap_dance_state_t *state, void *user_data);
+void cap_reset(qk_tap_dance_state_t *state, void *user_data);
+#endif
+
 enum custom_keycodes {
     BREAD = SAFE_RANGE,
     RTEST,
@@ -24,7 +49,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_smol(
-     KC_LCTL
+     TD(CAP_CTL)
  )
 };
 
@@ -59,3 +84,50 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 }
 #endif
 
+#ifdef TAP_DANCE_ENABLE
+uint8_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return SINGLE_TAP;
+        else return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->interrupted) return DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    }
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TRIPLE_TAP;
+        else return TRIPLE_HOLD;
+    } else return 8;
+}
+
+static tap captap_state = {
+    .is_press_action = true,
+    .state = 0
+};
+
+void cap_finished(qk_tap_dance_state_t *state, void *user_data) {
+    captap_state.state = cur_dance(state);
+    switch (captap_state.state) {
+        case SINGLE_TAP: register_code(KC_CAPS); break;
+        case SINGLE_HOLD: register_code(KC_LCTRL); break;
+        case DOUBLE_TAP: register_code(KC_SLCK); break;
+        case DOUBLE_HOLD: register_code(KC_LALT); break;
+        case DOUBLE_SINGLE_TAP: tap_code(KC_F); register_code(KC_X);
+    }
+}
+
+void cap_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (captap_state.state) {
+        case SINGLE_TAP: unregister_code(KC_CAPS); break;
+        case SINGLE_HOLD: unregister_code(KC_LCTRL); break;
+        case DOUBLE_TAP: unregister_code(KC_SLCK); break;
+        case DOUBLE_HOLD: unregister_code(KC_LALT);
+        case DOUBLE_SINGLE_TAP: unregister_code(KC_F);
+    }
+    captap_state.state = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [CAP_CTL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, cap_finished, cap_reset)
+};
+#endif
