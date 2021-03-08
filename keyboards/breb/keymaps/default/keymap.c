4ca29f848f891cb24face08d5b6d1720107bde72
ItsWaffIe <applej955@gmail.com>
2021-03-08 10:15:37 -0700

    Fix rotation

--- keyboards/breb/keymaps/default/keymap.c
+++ keyboards/breb/keymaps/default/keymap.c
@@ -70,9 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  )
 };
 
-#ifdef KEYLOGGER_ENABLE
 #include "oled.c"
-#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef MAIN
