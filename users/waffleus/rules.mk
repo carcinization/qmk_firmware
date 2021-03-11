05a27c56280caa53e358dfb66da8891798605524
ItsWaffIe <applej955@gmail.com>
2021-03-10 20:16:52 -0700

    rules.mk & config.h stuff for userspace

--- users/waffleus/rules.mk
+++ users/waffleus/rules.mk
@@ -1,15 +1,11 @@
 SRC += waffleus.c
 
 BOOTMAGIC_ENABLE = lite
-AUDIO_ENABLE = no
-GRAVE_ESC_ENABLE = no
 EXTRAKEY_ENABLE = yes
-CONSOLE_ENABLE = no
-SWAP_HANDS_ENABLE = no
-AUTO_SHIFT_ENABLE = no
-UNICODE_ENABLE = no
-SPACE_CADET_ENABLE = no
 MOUSEKEY_ENABLE = yes
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
 LTO_ENABLE = yes
 ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
     POINTING_DEVICE_ENABLE := yes
@@ -17,21 +13,39 @@ ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
     SRC += pimoroni_trackball.c
     QUANTUM_LIB_SRC += i2c_master.c
 endif
-ifeq ($(strip $(KEYLOGGER)), yes)
-	OPT_DEFS += -DKEYLOGGER
+
+ifeq ($(strip $(KEYBOARD)), crkbd/rev1/common)
+MCU = STM32F303
+CTPC = yes
+DEBUG_MATRIX_SCAN_RATE_ENABLE = yes
+SERIAL_DRIVER = usart
+WS2812_DRIVER = pwm
+#PWM needs patch to <https://github.com/qmk/qmk_firmware/blob/master/platforms/chibios/GENERIC_STM32_F303XC/configs/proton_c.mk>, otherwise defaults to bitbang.
+#See develop PR <https://github.com/qmk/qmk_firmware/pull/11864>
+VIA_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+WPM_ENABLE = yes
+COMMAND_ENABLE = yes
+UNICODEMAP_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
+CONSOLE_ENABLE = yes
+POINTING_DEVICE_ENABLE = no
+SPLIT_KEYBOARD = yes
+RGBLIGHT_ENABLE = yes
+NKRO_ENABLE = yes
 endif
-ifeq ($(strip $(UNICODEMAP_ENABLE)), yes)
-	SRC += unicode.c
-endif
-ifeq ($(strip $(COMBO_ENABLE)), yes)
-	SRC += combos.c
-endif
-ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
-	SRC += tapdance.c
-endif
-ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
-	SRC += oled.c
-endif
-ifeq ($(strip $(RGB_LIGHT_ENABLE)), yes)
-	SRC += rgb.c
+ifeq ($(strip $(KEYBOARD)), crkbd/rev1)
+MCU = atmega32u4
+BOOTLOADER = caterina
+#BOOTLOADER = nano-boot
+#BOOTLOADER_SIZE = 512
+RGBLIGHT_ENABLE = no
+AUDIO_ENABLE = no
+OLED_DRIVER_ENABLE = no
+GRAVE_ESC_ENABLE = no
+EXTRAKEY_ENABLE = no
+LTO_ENABLE = yes
+WPM_ENABLE = yes
+UNICODEMAP_ENABLE = no
+POINTING_DEVICE_ENABLE = no
 endif
