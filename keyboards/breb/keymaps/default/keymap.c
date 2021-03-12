81e4af07d3e60e0302ce2aa27c681bf02ed1e94f
ItsWaffIe <applej955@gmail.com>
2021-03-11 18:15:19 -0700

    Confirm working Protorne stuff :D

--- keyboards/breb/keymaps/default/keymap.c
+++ keyboards/breb/keymaps/default/keymap.c
@@ -54,7 +54,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_smol(
-     TG(1), TG(2), TG(3)
+     TG(1), KC_CAPS, KC_A
  ),
  [1] = LAYOUT_smol(
      TG(1), KC_A, KC_B
