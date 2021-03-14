25309099eba6f66af84bf441bb4f236f7f869315
ItsWaffIe <applej955@gmail.com>
2021-03-14 11:48:29 -0600

    Userspace

--- users/waffleus/tapdance.c
+++ users/waffleus/tapdance.c
@@ -1,18 +1,10 @@
 #pragma once
 
 enum {
-  HAP_SAD,
   QMK,
   DOCS,
+  HAP_SAD,
 };
-
-void dance_pep_finished(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-      SEND_STRING(":widepeepohappy1"SS_TAP(X_ENTER)SS_TAP(X_BSPC)":widepeepohappy2"SS_TAP(X_ENTER)SS_TAP(X_BSPC)SS_TAP(X_ENTER));
-    } else {
-      SEND_STRING(":widepeeposad1"SS_TAP(X_ENTER)SS_TAP(X_BSPC)":widepeeposad2"SS_TAP(X_ENTER)SS_TAP(X_BSPC)SS_TAP(X_ENTER));
-    }
-}
 
 void dance_qmk_finished(qk_tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
@@ -30,8 +22,16 @@ void dance_doc_finished(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+void dance_pep_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+      SEND_STRING(":widepeepohappy1"SS_TAP(X_ENTER)SS_TAP(X_BSPC)":widepeepohappy2"SS_TAP(X_ENTER)SS_TAP(X_BSPC)SS_TAP(X_ENTER));
+    } else {
+      SEND_STRING(":widepeeposad1"SS_TAP(X_ENTER)SS_TAP(X_BSPC)":widepeeposad2"SS_TAP(X_ENTER)SS_TAP(X_BSPC)SS_TAP(X_ENTER));
+    }
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [HAP_SAD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_pep_finished, NULL),
     [QMK] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_qmk_finished, NULL),
     [DOCS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_doc_finished, NULL),
+    [HAP_SAD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_pep_finished, NULL),
 };
