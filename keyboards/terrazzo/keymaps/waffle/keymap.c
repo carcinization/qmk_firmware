38af65a99d27ef03ecc268f7bc99093cd5e7571a
ItsWaffIe <applej955@gmail.com>
2021-03-15 22:44:26 -0600

    Add gamer layer

--- keyboards/terrazzo/keymaps/waffle/keymap.c
+++ keyboards/terrazzo/keymaps/waffle/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  	KC_MPLY, QMKTD,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  X(VIB),
 	    TZ_NXT,  ALT_TAB, SFA,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    SFL,            KC_BSPC,
 	    TZ_PRV,  CP_PSTE, CTLTZ,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, CTLDOT,         CTLSLSH,
-	    TZ_OFF,           KC_LALT, KC_LGUI,          LWRSPC,           RAISE,            KC_RGUI,  KC_RALT
+	    TZ_OFF,           KC_LALT, KC_LGUI,          LWRSPC,           RAISE,            KC_RGUI,  TG(_EXTRA)
     ),
 
     [_LOWER] = LAYOUT(
@@ -109,10 +109,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_EXTRA] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______,          _______, _______,          _______,          _______,          _______, _______
+        _______, KC_CAPS, KC_Q, KC_W, KC_E, KC_R, KC_T, _______, _______, _______, _______, _______, _______,
+        _______, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, _______, _______, _______, _______,          _______,
+        _______, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, _______, _______, _______, _______,          _______,
+        _______,          _______, KC_SPC,          KC_SPC,          _______,          _______, TG(_EXTRA)
     )
 };
 
