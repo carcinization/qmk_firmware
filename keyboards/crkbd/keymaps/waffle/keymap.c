4b2c02a328b10bc7d711619d6ec979cb3a3a75a5
toastedmangoes <applej955@gmail.com>
2021-03-16 19:23:22 -0600

    Working RGB on avr corne

--- keyboards/crkbd/keymaps/waffle/keymap.c
+++ keyboards/crkbd/keymaps/waffle/keymap.c
@@ -74,13 +74,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     //|---------------------------------------------------|                 |------------------------------------------------------|
-       QMKTD,    ALTQ,  KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,      KC_O,    ALTP,   MPSCR,
+       QMKTD,    ALTQ,  KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,      KC_O,    ALTP,   GPSCR,
     //|--------+--------+--------+--------+--------+------|                 |--------+--------+--------+--------+--------+---------|
-       MACTAB,   SFA,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,     KC_L,    SFCLN,   KC_MPLY,
+       ALT_TAB,  SFA,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,     KC_L,    SFCLN,   KC_MPLY,
     //---------+--------+--------+--------+--------+------|                 |--------+--------+--------+--------+--------+---------|
-       MAC_PSTE, CMDZ,  KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,   KC_M,     KC_COMM,  KC_DOT,  CMDSLSH, CMDS,
+       CP_PSTE,  CTLTZ, KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,   KC_M,     KC_COMM,  KC_DOT,  CTLSLH, CTLS,
     //---------+--------+--------+--------+--------+--------+----|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_MS_L, LOWER, RGB_TOG,   KC_BSPC, RAISE,   KC_MS_R
+                                           KC_MS_L, LOWER, KC_SPC,   KC_BSPC, RAISE,   KC_MS_R
                                       //|------------------------|  |--------------------------|
   ),
 
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+---------|                    |--------+--------+--------+--------+--------+-------------|
        KC_SLCK,  SFEXM,   KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN,  MAKE,
     //|--------+--------+--------+--------+--------+---------|                    |--------+--------+--------+--------+--------+-------------|
-       KC_NLCK,   CMDESC,  KC_TAB,  KC_CAPS,  KC_TILD,  KC_GRV,                       KC_LEFT,  KC_UP,    KC_DOWN,  KC_RGHT,  KC_RGUI, KC_MPRV,
+       KC_NLCK,   CTLESC,  KC_TAB,  KC_CAPS,  KC_TILD,  KC_GRV,                       KC_LEFT,  KC_UP,    KC_DOWN,  KC_RGHT,  KC_RCTL, KC_MPRV,
     //|--------+--------+--------+--------+--------+--------+-------|  |--------+--------+--------+--------+--------+--------+---------------|
                                            KC_LGUI, KC_TRNS, KC_SPC,    KC_VOLU,   KC_MS_U,  KC_PGUP
                                         //|-------------------------|  |----------------------------|
