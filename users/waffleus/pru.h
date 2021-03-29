a1e23b9939f0244404980b28675ca3bfb84cb017
ItsWaffIe <applej955@gmail.com>
2021-03-29 03:56:25 -0600

    Add initial procces_record_user stuff to userspace?

---
+++ users/waffleus/pru.h
@@ -0,0 +1,36 @@
+#pragma once
+
+enum typing_mode {
+  TM_NOMODE,
+  TM_REGIONAL,
+  TM_WIDE,
+  TM_SCRIPT,
+  TM_BLOCKS,
+  TM_AUSSIE,
+  TM_ZALGO,
+  TM_WOWMODE,
+  TM_D3MODE
+};
+
+enum custom_keycodes {
+  CP_PSTE = SAFE_RANGE,
+  UNIT,
+  UNIT2,
+  UNIT3,
+  UNIT4,
+  WEEB,
+  RGBRST,
+  RWORD,
+  SPONGE,
+  ALT_TAB,
+  MAKE,
+  KC_NOMODE,
+  KC_REGIONAL,
+  KC_WIDE,
+  KC_SCRIPT,
+  KC_BLOCKS,
+  KC_AUSSIE,
+  KC_ZALGO,
+  KC_WOWMODE,
+  KC_D3MODE
+};
