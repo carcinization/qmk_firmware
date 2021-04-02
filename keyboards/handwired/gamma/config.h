80d3189ad995907c6b2c25873e7898e3879b8114
ItsWaffIe <applej955@gmail.com>
2021-04-02 14:45:25 -0600

    Add encoder to Gamma

--- keyboards/handwired/gamma/config.h
+++ keyboards/handwired/gamma/config.h
@@ -4,10 +4,12 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Riley
-#define PRODUCT         preonictwogamma
+#define MANUFACTURER    RYKB
+#define PRODUCT         Matcha59
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 12
 #define MATRIX_ROW_PINS { F0, F1, C7, D5, B7 }
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5, B4, E6, D7 }
 #define DIODE_DIRECTION COL2ROW
+#define ENCODERS_PAD_A { D2 }
+#define ENCODERS_PAD_B { D3 }
--- keyboards/handwired/gamma/keymaps/default/keymap.c
+++ keyboards/handwired/gamma/keymaps/default/keymap.c
@@ -9,18 +9,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,      KC_MPLY, KC_RSFT,      MO(2),   KC_PGDN, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_MINS, KC_EQL,  _______,
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_MINS, KC_EQL,  _______,
         _______, KC_DEL,  KC_UP,   KC_ENT,  _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
         KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, KC_QUOT,
         _______, KC_PGDN, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,     _______, _______,      _______, _______, _______, _______
+        _______, _______, _______, _______, _______, RESET, _______, _______, _______, _______, _______
     ),
     [2] = LAYOUT(
-        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, KC_F12,  _______, _______, _______, KC_MINS, KC_EQL,  _______,
-        KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
-        KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_P1,   KC_P2,   KC_P3,   KC_ENT,  _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_P0,   KC_P0,   KC_PDOT, KC_ENT,      RESET,   _______,      _______, _______, _______, _______
+        KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,  _______, _______, KC_MINS, KC_EQL,  _______,
+        KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
+        KC_CAPS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PAST, _______, _______, _______, _______, _______, KC_QUOT,
+        KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_ENT,  _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_P0,   KC_P0,   KC_PDOT, KC_ENT,  KC_MUTE,   _______,      _______, _______, _______, _______
     ),
 
 };
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (IS_LAYER_ON(1)) {
+        tap_code((clockwise == true) ? KC_RBRC : KC_LBRC);
+    } else if (IS_LAYER_ON(2)) {
+      tap_code((clockwise == true) ? KC_MFFD : KC_MRWD);
+    } else {
+      tap_code((clockwise == true) ? KC_VOLU : KC_VOLD);
+    }
+}
