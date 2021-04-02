f5efc484fda9e042136957b1df75c0ff5bdabb65
ItsWaffIe <applej955@gmail.com>
2021-04-02 13:48:47 -0600

    Add Gamma stuff

---
+++ keyboards/handwired/gamma/config.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Riley
+#define PRODUCT         preonictwogamma
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 12
+#define MATRIX_ROW_PINS { F0, F1, C7, D5, B7 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5, B4, E6, D7 }
+#define DIODE_DIRECTION COL2ROW
