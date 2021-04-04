8252984bdad0960e4a178218f3f5fe66620b7f9e
ItsWaffIe <applej955@gmail.com>
2021-04-03 19:00:05 -0600

    Restar Felix stuff

--- keyboards/breb/keymaps/default/keymap.c
+++ keyboards/breb/keymaps/default/keymap.c
@@ -52,7 +52,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_smol(
-     RGB_HUD, RGB_HUI, RGB_VAD
+     KC_A, KC_B, KC_C
  ),
  [1] = LAYOUT_smol(
      TG(1), KC_A, KC_B
