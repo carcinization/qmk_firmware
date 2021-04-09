a60f68ac47369dfce2094f636786c34417230ac8
itswaffie <applej955@gmail.com>
2021-04-08 20:15:28 -0600

    Fix layout wrapper

--- keyboards/crkbd/rev1/common/keymaps/waffleus/keymap.c
+++ keyboards/crkbd/rev1/common/keymaps/waffleus/keymap.c
@@ -1,24 +1,24 @@
 #include "waffleus.h"
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_waffle(
+    [_QWERTY] = LAYOUT_waffle_corne(
         ________________CRKBDQWERTY1_______________,
         ________________CRKBDQWERTY2_______________,
         ________________CRKBDQWERTY3_______________,
         ________________CRKBDQWERTY4_______________
     ),
-    [_LOWER] = LAYOUT_waffle(
+    [_LOWER] = LAYOUT_waffle_corne(
         ________________CRKBDLOWER1________________,
         ________________CRKBDLOWER2________________,
         ________________CRKBDLOWER3________________,
         ________________CRKBDLOWER4________________
     ),
-    [_RAISE] = LAYOUT_waffle(
+    [_RAISE] = LAYOUT_waffle_corne(
         ________________CRKBDRAISE1________________,
         ________________CRKBDRAISE2________________,
         ________________CRKBDRAISE3________________,
         ________________CRKBDRAISE4________________
     ),
-    [_ADJUST] = LAYOUT_waffle(
+    [_ADJUST] = LAYOUT_waffle_corne(
         _______________CRKBDADJUST1________________,
         _______________CRKBDADJUST2________________,
         _______________CRKBDADJUST3________________,
