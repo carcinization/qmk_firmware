e7736f4f71ac7557b13b024a007b6dfb62543949
itswaffie <applej955@gmail.com>
2021-04-12 12:28:24 -0600

    Add Bongocat to userspace

--- users/waffleus/oled.h
+++ users/waffleus/oled.h
@@ -3,3 +3,9 @@
 #include "oled_driver.h"
 #define WPM "WPM:"
 bool process_record_user_oled(uint16_t keycode, keyrecord_t *record);
+#define BONGO_IDLE_FRAMES 5
+#define BONGO_SIZE 512
+#define BONGO_TAP_FRAMES 2
+#define BONGO_IDLE_SPEED 30
+#define BONGO_TAP_SPEED 40
+#define BONGO_FRAME_DURATION 200
