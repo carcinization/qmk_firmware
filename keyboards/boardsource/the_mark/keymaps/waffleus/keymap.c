0d945e271839565c8e244627167607f2831bef71
itswaffie <applej955@gmail.com>
2021-05-01 00:59:37 -0600

    via for tbk mini

--- keyboards/boardsource/the_mark/keymaps/waffleus/keymap.c
+++ keyboards/boardsource/the_mark/keymaps/waffleus/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		QMKTD,           ALTQ,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    ALTP,    XXX, XXX, XXX, KC_MNXT,
 		ALT_TAB,          SFA,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    SFL,    KC_BSPC, XXX,          XXX,  KC_MPRV,
 		CP_PSTE, CP_PSTE, CTLZ,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, CTLDOT,  CTLSLH, XXX,          KC_VOLU,
-	    KC_LCTL, KC_LALT, KC_LGUI,          LWRSPC,           KC_SPC,           RAISE,           KC_RGUI, KC_RALT, KC_RCTL,  RGB_RMOD, KC_VOLD, RGB_MOD
+	    KC_LCTL, KC_LALT, KC_LGUI,          LWRSPC,           CLIPST,           RAISE,           KC_RGUI, KC_RALT, KC_RCTL,  RGB_RMOD, KC_VOLD, RGB_MOD
 	),
     [_LOWER] = LAYOUT_all(
         XXX,      XXX,     XXX,      XXX,      XXX,      XXX,      XXX,      XXX,      XXX,      XXX,      XXX,     XXX,     XXX,     XXX,    XXX,    XXX,
