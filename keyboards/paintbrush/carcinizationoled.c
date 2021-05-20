a574b22409bd877abdd7c2119d018103e5868dc2
carcinization <80171031+carcinization@users.noreply.github.com>
2021-05-20 00:29:59 -0400

    it works!! oled
    artsey oled is alive

--- keyboards/paintbrush/carcinizationoled.c
+++ keyboards/paintbrush/carcinizationoled.c
@@ -26,16 +26,18 @@ const char brac_map[]= "{)( \n"
 const char shifted_brac_map[]= "{)( \n"
 "}}{}";
 
-static char const nav_map[] PROGMEM = {0x1E, 0x11, 0x18, 0x10, 0, 0, 0x1F, 0x1B, 0x19, 0x1A, 0
+static char const nav_map[] PROGMEM = {0x1E, 0x11, 0x18, 0x10, 0x20, 0x1F, 0x1B, 0x19, 0x1A, 0
 	};
-
+static char const mou_map[] PROGMEM = {0x1E, 0x32, 0x18, 0x31, 0x20, 0x1F, 0x1B, 0x19, 0x1A, 0
+	};
 
 void oled_task_user(void) {
     // Host Keyboard Layer Status
-    oled_write_P(PSTR("Map: "), false);
+    
 	
     switch (get_highest_layer(layer_state)) {
         case _A_BASE:
+			oled_write_P(PSTR("_BASE"), false);
 			if (get_mods() & MOD_BIT(KC_LSFT)) {
 				oled_write(shifted_base_map, false);
 			} 
@@ -45,12 +47,21 @@ void oled_task_user(void) {
             oled_write(base_map, false);}
             break;
         case _A_NUM:
-            oled_write_P(PSTR("NUM"), false);
+			oled_write_P(PSTR("_NUM "), false);
+            if (get_mods() & MOD_BIT(KC_LSFT)) {
+				oled_write(shifted_num_map, false);
+			} 
+			else if (get_oneshot_mods() & MOD_BIT(KC_LSFT)){
+				oled_write(shifted_num_map, false);
+			} else {
+            oled_write(num_map, false);}
             break;
         case _A_NAV:
-           oled_write_P(nav_map, false);
+			oled_write_P(PSTR("_NAV "), false);
+            oled_write_P(nav_map, false);
             break;
 		case _A_SYM:
+			oled_write_P(PSTR("_SYM "), false);
 			if (get_mods() & MOD_BIT(KC_LSFT)) {
 				oled_write(shifted_sym_map, false);
 			} 
@@ -60,7 +71,8 @@ void oled_task_user(void) {
             oled_write(sym_map, false);}
 			break;
 		case _A_BRAC:
-		if (get_mods() & MOD_BIT(KC_LSFT)) {
+			oled_write_P(PSTR("_BRAC"), false);
+		    if (get_mods() & MOD_BIT(KC_LSFT)) {
 				oled_write(shifted_brac_map, false);
 			} 
 			else if (get_oneshot_mods() & MOD_BIT(KC_LSFT)){
@@ -68,13 +80,20 @@ void oled_task_user(void) {
 			} else {
             oled_write(brac_map, false);}
             break;
-		// case _A_MOU:
-		// case _A_CUSTOM:
+		case _A_MOU:
+			oled_write_P(PSTR("_MOU "), false);
+			oled_write_P(mou_map, false);
+			break;
+		case _A_CUSTOM:
+		        oled_write_P(PSTR("_CUS "), false);
+			    oled_write_ln_P(PSTR("Undefined"), false);
+			break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("Undefined"), false);
+            oled_clear();
     }
 }
+
 
 
 #endif
