99ef475347c6329cede6dcbc776ae21b054f18b5
carcinization <80171031+carcinization@users.noreply.github.com>
2021-08-13 08:19:00 -0400

    artsey merge i think?

---
+++ keyboards/faunchpad/keymaps/default_lefty/keymap.c
@@ -0,0 +1,11 @@
+#include "paintbrush.h"
+#include "defaultoled.c"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	LAYOUT(
+		KC_Q, KC_W, KC_E, KC_R, 
+		KC_A, KC_S, KC_D, KC_F),
+
+};
+
---
+++ keyboards/ferris/keymaps/bigartseycarcinization/mymacros.c
@@ -0,0 +1,12 @@
+/*     INCLUDE THIS IN artsey.h:
+
+	*/
+
+
+ case TOFN:
+        if (record->event.pressed) {
+            layer_move(_FN);;
+        } else {
+		}
+        break;
+
